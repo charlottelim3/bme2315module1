49c700e60e9ea94102617c5ed6215b7d36d804f9
sophotop <sophiazhou54@gmail.com>
2026-09-10 10:37:37 -0400

    my data sort
    This is my column sort for the data

---
+++ main_Sophia.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_csv("/Users/sophiazhou/Desktop/BME2315/Module 1/bme2315module1/Metadata and Protein Data for Module 1.csv")
+
+for column in df.columns:
+    print(column)
