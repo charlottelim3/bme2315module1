5bef6d84704fcd2c975d1d4f725b7bf16740d00c
sophotop <sophiazhou54@gmail.com>
2026-09-18 20:43:00 -0400

    updste data and graphs for my mod 1

--- main_Sophia.py
+++ main_Sophia.py
@@ -1,3 +1,6 @@
+# Sophia Zhou # 
+# Computing ID : ezy2hv #
+
 import pandas as pd
 
 df = pd.read_csv("/Users/sophiazhou/Desktop/BME2315/Module 1/bme2315module1/Metadata and Protein Data for Module 1.csv")
